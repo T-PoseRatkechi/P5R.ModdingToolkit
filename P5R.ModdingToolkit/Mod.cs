708e8d5b0d24d67def83421f6e2fd2e4c2f73e2d
T-Pose Ratkechi <tposeratkechi@gmail.com>
2024-01-27 06:26:30 -0600

    Update project files.

--- P5R.ModdingToolkit/Mod.cs
+++ P5R.ModdingToolkit/Mod.cs
@@ -1,44 +1,52 @@
-﻿using P5R.ModdingToolkit.Configuration;
+﻿using P5R.ModdingToolkit.Battles;
+using P5R.ModdingToolkit.Configuration;
 using P5R.ModdingToolkit.Template;
 using Reloaded.Hooks.ReloadedII.Interfaces;
+using Reloaded.Memory.SigScan.ReloadedII.Interfaces;
 using Reloaded.Mod.Interfaces;
+using System.Drawing;
 
-namespace P5R.ModdingToolkit
+namespace P5R.ModdingToolkit;
+
+public class Mod : ModBase
 {
-    public class Mod : ModBase
+    private readonly IModLoader modLoader;
+    private readonly IReloadedHooks? hooks;
+    private readonly ILogger log;
+    private readonly IMod owner;
+
+    private Config config;
+    private readonly IModConfig modConfig;
+    private readonly Encounters encounters = new();
+
+    public Mod(ModContext context)
     {
-        private readonly IModLoader modLoader;
-        private readonly IReloadedHooks? hooks;
-        private readonly ILogger log;
-        private readonly IMod owner;
+        this.modLoader = context.ModLoader;
+        this.hooks = context.Hooks!;
+        this.log = context.Logger;
+        this.owner = context.Owner;
+        this.config = context.Configuration;
+        this.modConfig = context.ModConfig;
 
-        private Config config;
-        private readonly IModConfig modConfig;
+        Log.Initialize("Modding Toolkit", this.log, Color.Cyan);
 
-        public Mod(ModContext context)
-        {
-            this.modLoader = context.ModLoader;
-            this.hooks = context.Hooks;
-            this.log = context.Logger;
-            this.owner = context.Owner;
-            this.config = context.Configuration;
-            this.modConfig = context.ModConfig;
-        }
-
-        #region Standard Overrides
-        public override void ConfigurationUpdated(Config configuration)
-        {
-            // Apply settings from configuration.
-            // ... your code here.
-            config = configuration;
-            log.WriteLine($"[{modConfig.ModId}] Config Updated: Applying");
-        }
-        #endregion
-
-        #region For Exports, Serialization etc.
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-        public Mod() { }
-#pragma warning restore CS8618
-        #endregion
+        this.modLoader.GetController<IStartupScanner>().TryGetTarget(out var scanner);
+        this.encounters.Initialize(scanner!, this.hooks);
     }
+
+    #region Standard Overrides
+    public override void ConfigurationUpdated(Config configuration)
+    {
+        // Apply settings from configuration.
+        // ... your code here.
+        config = configuration;
+        log.WriteLine($"[{modConfig.ModId}] Config Updated: Applying");
+    }
+    #endregion
+
+    #region For Exports, Serialization etc.
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    public Mod() { }
+#pragma warning restore CS8618
+    #endregion
 }
